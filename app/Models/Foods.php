8b62be4991b042d30362c0664ce1e0321efa9b11
DmitryErofeev <u1758652@unimail.hud.ac.uk>
2020-10-13 17:22:07 +0100

    Added FoodFactory to generate test data. Updated migration tables and models to include foreign keys and relationships

--- app/Models/Foods.php
+++ app/Models/Foods.php
@@ -11,4 +11,7 @@ class Foods extends Model
 
     protected $fillable  = ["name", "description"];
 
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 }
--- database/migrations/2020_10_07_173549_create_foods_table.php
+++ database/migrations/2020_10_07_173549_create_foods_table.php
@@ -15,9 +15,15 @@ class CreateFoodsTable extends Migration
     {
         Schema::create('foods', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger("user_id");
             $table->string("name");
             $table->string("description");
             $table->timestamps();
+
+            $table->foreign("user_id")
+            ->references("id")
+            ->on("users")
+            ->onDelete("cascade");
         });
     }
 
